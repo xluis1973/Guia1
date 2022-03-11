dec6e22146b7e8efc146cd0cd75e735f691aa9b3
xluis1973 <50953075+xluis1973@users.noreply.github.com>
2022-03-11 19:17:02 -0300

    Ej20
    Modificación en ejercicio 20

--- src/guia1/Guia1_Ej20.java
+++ src/guia1/Guia1_Ej20.java
@@ -5,6 +5,8 @@
  */
 package guia1;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -20,41 +22,25 @@ public class Guia1_Ej20 {
         // TODO code application logic here
         int lista[]=new int[100];
         rellenaVector(lista);
+        //Visualizados en orden descendente.
         for(int i=99;i>-1;i--){
         
             System.out.println(lista[i]);
         }
+        
+        
     }
  
     public static void rellenaVector(int lista[]){
     
         int nro=1;
         int contador=0;
-        while(contador<100){
+        for(int i=0;i<lista.length;i++){
         
-            if(esPrimo(nro)){
-            
-                lista[contador]=nro;
-                contador++;
-            }
-            nro++;
+            lista[i]=nro++;
         }
         
     }
     
-    public static boolean esPrimo(int nro){
     
-        if(nro==1 || nro==2){
-        
-            return true;
-        }
-        for(int i=2;i<nro;i++){
-        
-            if(nro%i==0){
-            
-                return false;
-            }
-        }
-        return true;
-    }
 }
